440498d714fae8bc7b57753900b65a00c7063a95
Porfirio Partida - home <porfirio.partidai@gmail.com>
2015-09-29 22:19:35 -0700

    Adding assets for spamranha and wakeup

--- src/main/java/com/fifino/gptw/helpers/GPTWGamesPool.java
+++ src/main/java/com/fifino/gptw/helpers/GPTWGamesPool.java
@@ -6,8 +6,11 @@ package com.fifino.gptw.helpers;
 public final class GPTWGamesPool {
 
     public static final Class[] LIST = new Class[]{
-            com.fifino.gptw.screens.games.TurnOffAirs.class,
-            com.fifino.gptw.screens.games.TurnOffAlarms.class,
-            com.fifino.gptw.screens.games.FindTheCar.class
+//            com.fifino.gptw.screens.games.TurnOffAirs.class,
+//            com.fifino.gptw.screens.games.TurnOffAlarms.class,
+//            com.fifino.gptw.screens.games.FindTheCar.class,
+
+//            com.fifino.gptw.screens.games.WakeUp.class,
+            com.fifino.gptw.screens.games.Spamranhas.class,
     };
 }
