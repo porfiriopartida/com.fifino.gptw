11e789ae908ceb22ac388e0743366d7046d943fe
Porfirio Partida - home <porfirio.partidai@gmail.com>
2016-05-25 19:35:39 -0700

    App rename + new unit

--- src/test/java/com/fifino/gptw/helpers/GPTWGamesPoolTest.java
+++ src/test/java/com/fifino/gptw/helpers/GPTWGamesPoolTest.java
@@ -18,4 +18,11 @@ public class GPTWGamesPoolTest {
         assertTrue(GPTWGamesPool.LIST.length > 0);
     }
 
+    @Test
+    public void testGameTypes(){
+        assertEquals(GPTWGamesPool.LIST[GPTWGamesPool.SPAMRANHAS].toString(), com.fifino.gptw.screens.games.Spamranhas.class.toString() );
+        assertEquals(GPTWGamesPool.LIST[GPTWGamesPool.TURN_OFF_AIRS].toString(), com.fifino.gptw.screens.games.TurnOffAirs.class.toString() );
+        assertEquals(GPTWGamesPool.LIST[GPTWGamesPool.TURN_OFF_ALARMS].toString(), com.fifino.gptw.screens.games.TurnOffAlarms.class.toString() );
+        assertEquals(GPTWGamesPool.LIST[GPTWGamesPool.FIND_THE_CAR].toString(), com.fifino.gptw.screens.games.FindTheCar.class.toString() );
+    }
 }
